fe8670aae22cfd2bf624dcd8d9b56efc5e085a2a
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-02 11:37:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  new_data_structure = data.each_with_object({}) {|(first_k,first_v), a|
+  binding.pry
+    v.each{|second_k,second_v|
+      p "This runs"
+      p "value: #{first_v}"
+      p "key: #{first_k}"
+    }
+  }
+  
+
+  # Search for and input data into new structure
+  new_data_structure.each{|key,value|
+    # count = 0
+    data.each{|basic_characteristic|
+        spec_trait = ''
+        basic_characteristic.each{|specific_traits|
+            if specific_traits.class == Symbol
+                spec_trait = specific_traits
+            end
+            if specific_traits.class != Symbol
+                specific_traits.each{|trait_variant, names|
+                    if names.include?(key) && spec_trait.class == Symbol
+                      # p trait_variant
+                      # p trait_variant.to_s
+                      new_data_structure[key][spec_trait].push(trait_variant.to_s)
+                    end
+                }
+            end
+        }
+    }
+}
+
+
+# End of method
 end
+ 
